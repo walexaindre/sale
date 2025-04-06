fafce60ddc9cfb2afe2608c9dae67b7940de35b0
walexaindre <walexaindre@hotmail.com>
2025-04-06 10:34:48 -0400

    start

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Item from "./lib/Item.svelte";
+  import Mesh from "./assets/grid.svg?raw";
+  import * as Cons from "./Constants";
+</script>
+
+<div class="navbar text-neutral-content z-20 sticky top-0 bg-base-100">
+  <div class="navbar-start">
+    <button class="btn btn-square btn-ghost mx-2">
+      {@html Mesh}
+    </button>
+  </div>
+  <div class="navbar-center">
+    <label class="input">
+      <span class="label">Search</span>
+      <input type="text" placeholder="Type something" />
+    </label>
+  </div>
+  <div class="navbar-end"></div>
+</div>
+
+<div class="grid grid-cols-[repeat(auto-fill,minmax(384px,1fr))] gap-4 p-4">
+  <Item name="alpha" img="luggage.svg" imgdesc="sale" price="300"></Item>
+</div>
+
+<div class="flex justify-center">
+  <div class="join">
+    <button class="join-item btn">«</button>
+    <button class="join-item btn">Page 22</button>
+    <button class="join-item btn">»</button>
+  </div>
+</div>
---
+++ src/lib/Item.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    let {
+        name,
+        description = "quo erat demostratum",
+        img,
+        imgdesc,
+        price,
+        quantity = 0,
+        tags = ["alpha", "beta prunning", "eta gama eta"],
+        category = "a",
+        condition = "new",
+    } = $props();
+    function a() {
+        console.log("hello");
+    }
+</script>
+
+<div class="flex justify-center">
+    <div class="card card-dash bg-base-300 w-96 shadow-sm">
+        <div class="card-body">
+            <button class="btn btn-ghost h-fit relative">
+                <figure>
+                    <img src={img} alt={imgdesc} />
+                    {#if quantity == 0}
+                        <div
+                            class=" absolute text-5xl text-error bg-error-content w-full py-2 -rotate-45 text-center"
+                        >
+                            Sold!
+                        </div>
+                    {/if}
+                </figure>
+            </button>
+            <div class="flex justify-between">
+                <h2 class="text-2xl font-bold">{name}</h2>
+                {#if quantity > 0}
+                    <span class="text-2xl text-secondary">${price}</span>
+                {:else}
+                    <span class="text-2xl text-secondary line-through"
+                        >${price}</span
+                    >
+                {/if}
+            </div>
+            <p>
+                {description}
+            </p>
+            <div class="flex flex-wrap gap-2">
+                {#if condition == "new"}
+                    <div class="badge badge-info">New</div>
+                {:else if condition == "used"}
+                    <div class="badge badge-info">Used</div>
+                {:else}
+                    <div class="badge badge-info">Refurbished</div>
+                {/if}
+                {#if category}
+                    <div class="badge badge-soft badge-warning">
+                        {category}
+                    </div>
+                {/if}
+                {#each tags as tag}
+                    <div class="badge badge-outline badge-primary">
+                        {tag}
+                    </div>
+                {/each}
+                {#if quantity > 1}
+                    <div class="badge badge-outline badge-success">
+                        {quantity} units available
+                    </div>
+                {:else if quantity == 1}
+                    <div class="badge badge-outline badge-warning">
+                        Only one left
+                    </div>
+                {:else}
+                    <div class="badge badge-error">Out of stock</div>
+                {/if}
+            </div>
+        </div>
+    </div>
+</div>
